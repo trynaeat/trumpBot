0422429bfbf13cd4ec5c6b06fee23d65e5ecdab0
jakparks <jakparks@cisco.com>
2017-03-05 01:39:02 -0500

    HTMLify links

--- client/js/pages/home/home.js
+++ client/js/pages/home/home.js
@@ -1,8 +1,21 @@
 app.controller('homeController', [ '$scope', '$http', function($scope, $http) {
   function _getTweet() {
     $http.get('/tweet').then(function(result) {
-      $scope.tweet = result.data.tweet;
+      $scope.tweet = _htmlifyLinks(result.data.tweet);
     });
+  }
+
+  function _htmlifyLinks(tweet) {
+    var tokens = tweet.split(' ');
+    for(var i = 0; i < tokens.length; i++) {
+      if(tokens[i].startsWith('https://')) {
+        tokens[i] = '<a href="' + tokens[i] + '">' + tokens[i] + '</a>';
+      }
+      if(tokens[i].startsWith('@')) {
+        tokens[i] = '<a>' + tokens[i] + '</a>';
+      }
+    }
+    return tokens.join(' ');
   }
   _getTweet();
   var today = new Date();
